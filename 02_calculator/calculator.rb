d9642a510953dd43b2dc64e2d163aeeca9a2c190
Scott Huang <scottyh527@gmail.com>
2019-05-13 19:22:56 -0400

    completed calculator tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -12,6 +12,25 @@ def sum(array)
   array.reduce(0, :+)
 end
 
-def multiply(x,y)
-  return x * y
+def multiply(*numbers)
+  numbers.inject(:*)
+end
+
+def power(x,y)
+  x**y
+end
+
+def factorial(num)
+  if num == 0
+    return 1
+  elsif num > 0
+    i = 1
+    total = num
+    while i < num do
+      total *= i
+      i+=1
+    end
+    puts num
+    return total
+  end
 end
