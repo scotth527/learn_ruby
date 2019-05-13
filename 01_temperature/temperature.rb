20d5eeb8937a6cb5bceb0f177e9027fbab1b08f3
Scott Huang <scottyh527@gmail.com>
2019-05-12 21:30:56 -0400

    completed some exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
+
 #write your code here
+
+def ftoc(degree)
+  return (degree - 32.0) * (5.0/9.0)
+end
+
+def ctof(degree)
+  return (degree * 9.0/5.0) + 32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
+
 #write your code here
+def add (x,y)
+  return x + y
+end
+
+def subtract(x,y)
+  return x - y
+end
+
+def sum(array)
+  array.reduce(0, :+)
+end
+
+def multiply(x,y)
+  return x * y
+end
