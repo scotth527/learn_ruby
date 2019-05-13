1f2d8776f2ccce564ca5b4949e4c7dfb2d46d48f
Scott Huang <scottyh527@gmail.com>
2019-05-13 19:24:08 -0400

    completed simon says exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,38 @@
 #write your code here
+def echo(string)
+  return string
+end
+
+def shout(string)
+  return string.upcase
+end
+
+def repeat(string, int = 2)
+  array = []
+  i = 0
+  while i < int do
+    array.push(string)
+    i += 1
+  end
+  return array.join(' ')
+end
+
+def start_of_word(string, index = 1)
+  return string.slice(0..(index-1))
+end
+
+def first_word(string, index=1)
+  return string.split(' ')[0]
+end
+
+def titleize(string)
+  string = string.split(' ').map.with_index{|word,idx|
+    if word == 'and' || word == 'the' || word == 'over'
+      word
+    else
+      word.capitalize
+    end
+  }
+  string[0].capitalize!
+	string.join(" ")
+end
